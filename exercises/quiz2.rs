31f02f78dd6808a0ce67bfa2566f237ed30b62cc
Your Name <you@example.com>
2024-10-13 09:06:13 +0800

    wu

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -29,14 +29,21 @@ pub enum Command {
 }
 
 mod my_module {
+    use std::{fmt::format, process::Output};
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
+        
+        let mut output:Vec<String>  = Vec::with_capacity(input.len());
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let str_last=match command{
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => format!("{}{}",string,"bar".repeat(*n)),
+            };
+            output.push(str_last);
         }
         output
     }
@@ -45,7 +52,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
